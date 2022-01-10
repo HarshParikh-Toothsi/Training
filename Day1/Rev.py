d788234ad52a9ff137f3c813f6c233a84b1ce767
Harsh <harsh.parikh@toothsi.in>
2022-01-10 19:48:57 +0530

    Negative numbers are handled in Fibonacci Series

--- Day1/Rev.py
+++ Day1/Rev.py
@@ -1,9 +1,14 @@
 def rev(n):
+    if n<0:
+        flag=1;
+        n= n*(-1)
     a = n
     ans=0
     while a>0:
         ans = (ans*10)+ (a%10)
         a = a//10
+    if(flag==1):
+        ans = ans *(-1)
     print(ans)
 #reverse of given number
 rev(456) # n=456 # n = int(input())
